b8e39cc119fc2a193329f45eb7c70a80e339e10c
lukasz.laszkiewicz <lukasz.laszkiewicz@capgemini.com>
2023-05-18 13:46:09 +0200

    feat: initial data setup, timeout fix

--- setup-environment.sh
+++ setup-environment.sh
@@ -12,6 +12,7 @@ echo 'waiting for pods to start before starting next...';
 time docker-compose up -d data-mgmt 
 echo 'waiting for pods to start before starting next...';
 
+sleep 10
 echo 'setting up data-mgmt-ui ';
 # WSL
 cat ./data-mgmt-init/data_mgmt_local.sql | docker exec -e PGPASSWORD=datamgmt -i app-docker-dev_postgresdb_1 psql -U datamgmt -d datamgmt
