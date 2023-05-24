f4fb84f1e605099b716593a9fdde5fc95d1f2383
lukasz.laszkiewicz <lukasz.laszkiewicz@capgemini.com>
2023-05-24 11:44:48 +0200

    feat: keycloak 18

--- setup-environment.sh
+++ setup-environment.sh
@@ -6,13 +6,10 @@ echo Y | docker-compose rm
 docker-compose up -d traefik postgresdb pgadmin
 echo 'waiting for pods to start before starting next...';
 
-docker-compose up -d tkit-portal-server
-echo 'waiting for pods to start before starting next...';
-
 time docker-compose up -d data-mgmt 
 echo 'waiting for pods to start before starting next...';
 
-sleep 10
+sleep 15
 echo 'setting up data-mgmt-ui ';
 # WSL
 cat ./data-mgmt-init/data_mgmt_local.sql | docker exec -e PGPASSWORD=datamgmt -i app-docker-dev_postgresdb_1 psql -U datamgmt -d datamgmt
